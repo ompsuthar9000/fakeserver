d08a42dae721235f2e3b41f0d947c2cf1a1d4da7
ompsuthar9000 <91866686+ompsuthar9000@users.noreply.github.com>
2024-02-24 11:01:54 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,58 @@
+export const data = [
+    {
+        "product_id": "1",
+        "product_name": "Adicolor Classics Joggers",
+        "price": 63.85,
+        "category": "Dress",
+        "image_source": "https://images.bestsellerclothing.in/data/only/update/june-12-2023/269176302_g1.jpg?width=1080&height=1355&mode=fill&fill=blur&format=auto"
+    },
+    {
+        "product_id": "2",
+        "product_name": "Nike Sportswear Futura Luxe",
+        "price": 130.00,
+        "category": "Bag",
+        "image_source": "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/40bd94cc-3303-40c3-a7a9-6bcb72f9b744/sportswear-futura-luxe-tote-7c904R.png"
+    },
+    {
+        "product_id": "3",
+        "product_name": "Geometric print Scarf",
+        "price": 53.99,
+        "category": "scarf",
+        "image_source": "https://rukminim2.flixcart.com/image/850/1000/l3hmwsw0/scarf/1/6/n/one-size-ch03sc022264-cloth-haus-india-original-imagehmzjkhahbha.jpeg?q=20&crop=false"
+    },
+    {
+        "product_id": "4",
+        "product_name": "Yellow Reserved Hoodie",
+        "price": 155.99,
+        "category": "Hoodies",
+        "image_source": "https://slayking.in/wp-content/uploads/2023/06/IMG_2750.jpg"
+    },
+    {
+        "product_id": "5",
+        "product_name": "Basic Dress Green",
+        "price": 59.99,
+        "category": "Dress",
+        "image_source": "https://pastelshades.in/wp-content/uploads/2022/07/38-4-1.jpg"
+    },
+    {
+        "product_id": "6",
+        "product_name": "Nike Air Zoom Pegasus",
+        "price": 179.99,
+        "category": "shoe",
+        "image_source": "https://assets.ajio.com/medias/sys_master/root/20230306/4v6o/6405c302f997dde6f4dd3cb5/-473Wx593H-469434690-blue-MODEL.jpg"
+    },
+    {
+        "product_id": "7",
+        "product_name": "Nike Repel Miler",
+        "price": 189.99,
+        "category": "Dress",
+        "image_source": "https://cdn.plutosport.com/m/catalog/product/N/i/Nike-Repel-Miler-Hardloopjack-Heren-2307311054.jpg?profile=product_page_image_medium&3=2"
+    },
+    {
+        "product_id": "8",
+        "product_name": "Polarised Sunglasses",
+        "price": 149.99,
+        "category": "Glasses",
+        "image_source": "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/9548589/2020/2/5/5be67465-ecce-44d7-9b61-d29685c9f7141580898377248-Fastrack-Men-Rectangle-Polarised-Sunglasses-NBP192BK4P-51715-1.jpg"
+    }
+]
